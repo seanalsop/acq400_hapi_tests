1b5c1565fcef8bc6b05e52a4d39d0c70bddebfd8
Peter Milne <peter.milne@d-tacq.com>
2017-07-03 14:03:16 +0100

    --trg int|ext control 	modified:   acq1014_caploop.py

--- acq1014_caploop.py
+++ acq1014_caploop.py
@@ -4,12 +4,12 @@
     acq1001_caploop UUT1 [UUT2 ..]
     where UUT1 is the ip-address or host name of first uut
     example test client runs captures in a loop on one or more uuts
-    
+
     pre-requisite: UUT's are configured and ready to make a transient
     capture 
     eg clk is running. soft trg enabled
     eg transient length set.
-    
+
     loop continues "forever" until <CTRL-C>
 """
 
@@ -29,7 +29,7 @@ def run_loop(args):
     if SAVEDATA:
         for u in uuts:
             u.save_data = SAVEDATA
-            
+
     for uut in uuts:
         if hasattr(uut.s0, 'TIM_CTRL_LOCK'):
             print "LOCKDOWN {}".format(uut)
@@ -39,13 +39,16 @@ def run_loop(args):
 
     try:
         while True:
-            shot_controller.run_shot(soft_trigger=1,acq1014_ext_trigger=3)
+            if args.trg == "int":
+                shot_controller.run_shot(soft_trigger=1,acq1014_ext_trigger=1)
+            else:
+                shot_controller.run_shot(soft_trigger=0,acq1014_ext_trigger=3)
             if args.sleep >= 0:
                 acq400_hapi.cleanup.sleep(args.sleep)            
             else:
                 print("Hit return when ready")
                 sys.stdin.readline()
-            
+
     except acq400_hapi.cleanup.ExitCommand:
         print("ExitCommand raised and caught")
         shot_controller.abort_shot()
@@ -56,6 +59,7 @@ def run_loop(args):
 def run_main():
     parser = argparse.ArgumentParser(description='acq1014 looptest')
     parser.add_argument('--sleep', default=1, type=int, help="sleep time between shots, -1:pause input")
+    parser.add_argument('--trg', default='int', type=str, help="trigger int|ext")
     parser.add_argument('uuts', nargs='+', help="uut pairs: m1,m2 [s1,s2 ...]")
     run_loop(parser.parse_args())
 
