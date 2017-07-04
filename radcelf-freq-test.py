9d1602ccae8903340c7ca9ec1d4a50656741262a
Peter Milne <peter.milne@d-tacq.com>
2017-07-04 14:27:08 +0100

    measure internal freq, scales with ext 50MHz clock 	modified:   radcelf-freq-test.py

--- radcelf-freq-test.py
+++ radcelf-freq-test.py
@@ -24,21 +24,32 @@ def freq(sig):
     return float(sig.split(" ")[1])
 
 FINT = float(300000000)
+CMULT = 12.0
+DBG = 0
 
 def init_clk(uut):
+    global FINT
 # Set AD9854 clock remap to 25 MHz
     uut.ddsC.CR     = '004C0041'
-    uut.ddsC.FTW1   = FTW1(1.0/12.0)
+    uut.ddsC.FTW1   = FTW1(1.0/CMULT)
 # Program AD9512 secondary clock to choose 25 MHz from the AD9854 remap
     uut.clkdB.CSPD  = '02'
     uut.clkdB.UPDATE = '01'
+    print("sleep 10 for FINT stabilization")
+    time.sleep(10)
+    FINT=round(freq(uut.s2.SIG_DDS_INP_FREQ)*CMULT, 4)
+    print("Using FINT=%.3e" % FINT)
     
     
 def set_freq(uut, dds, freq):
     global FINT 
+    global DBG
 # X12, SINC off    
     dds.CR   = '004C0041'
     dds.FTW1 = FTW1(freq/FINT)
+    if DBG:
+        print("set_freq %s %.3e FINT %.3e FTW1 %s" % ("ddsA" if uut.ddsA == dds else "ddsB",
+                                                      freq, FINT, FTW1(freq/FINT)))
 
 
 def valid_freq(actual, target):    
@@ -55,7 +66,7 @@ def verify_freq(uut, test, freqA, freqB):
             time.sleep(1)
             retry = retry + 1
             
-    print("test:%d FAIL %s %s" % (test, freq(uut.s2.SIG_DDS_A_FREQ),freq(uut.s2.SIG_DDS_B_FREQ)))
+    print("test:%d FAIL %s != %s %s != %s" % (test, freqA, freq(uut.s2.SIG_DDS_A_FREQ), freqB, freq(uut.s2.SIG_DDS_B_FREQ)))
     return False
     
 FREQS_MHz = [ 13, 17, 19, 23, 29, 31 ]    
@@ -71,6 +82,8 @@ def next_freq(f):
     
 def run_test(args):
     global FREQS_MHz
+    global DBG
+    DBG = args.debug
     uut = acq400_hapi.RAD3DDS(args.uut[0])
     
     uut.s2.RADCELF_init = 1
@@ -91,6 +104,7 @@ def run_test(args):
 def run_main():  
     parser = argparse.ArgumentParser(description='radcelf-chirp-init')
     parser.add_argument('--test', default=1, type=int, help="set number of tests to run")
+    parser.add_argument('--debug', default=0, type=int, help="set number of tests to run")
     parser.add_argument('uut', nargs=1, help="uut")
     run_test(parser.parse_args())
     
