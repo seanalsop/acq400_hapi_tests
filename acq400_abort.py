23569c057c73553447f03b1a67d47441d0c2be54
dt100 <dt100@brotta.d-tacq>
2017-07-27 09:50:31 +0100

    stops streaming case as well as transient  Committer: dt100 <dt100@brotta.d-tacq> 	modified:   acq400_abort.py

--- acq400_abort.py
+++ acq400_abort.py
@@ -2,11 +2,21 @@
 
 import acq400_hapi
 import os
+import argparse
 
-uuts = [ acq400_hapi.Acq400(u) for u in os.getenv("UUTS").split(" ")]
+parser = argparse.ArgumentParser(description='configure acq400_abort')
+parser.add_argument('uut', nargs='+', help="uut")
+
+args = parser.parse_args()
+uuts = [ acq400_hapi.Acq400(u) for u in args.uut ]
+
 
 for u in uuts:
 	u.s0.TIM_CTRL_LOCK = 0
+	u.s0.streamtonowhered = 'stop'
 	u.s0.set_abort = 1
+
+
+
 
 
