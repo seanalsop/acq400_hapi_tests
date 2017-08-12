93f8b8037c494c3fd90e8c2c290022dd0122d8ba
Peter Milne <peter.milne@d-tacq.com>
2017-08-12 20:29:34 +0100

    Rainbow test works .. to 16384 samples at least 	modified:   acq1001_awg_demo.py

--- acq1001_awg_demo.py
+++ acq1001_awg_demo.py
@@ -5,6 +5,7 @@ import acq400_hapi
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import timeit
 
 class RunsFiles:
     def __init__(self, uut, files):
@@ -16,6 +17,53 @@ class RunsFiles:
             with open(f, mode='r') as fp:
                 self.uut.load_awg(fp.read())
             yield f        
+
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped        
+
+class RainbowGen:
+    NCYCLES = 5
+    def offset(self, ch):
+        return -9.0 + 8.0*ch/self.nchan;
+    
+    def rainbow(self, ch):
+        return np.add(self.sw, self.offset(ch))
+    
+    def sin(self):
+        nsam = self.nsam
+        NCYCLES = self.NCYCLES    
+        return np.sin(np.array(range(nsam))*NCYCLES*2*np.pi/nsam)   # sin, amplitude of 1 (volt)
+        
+    def sinc(self, ch):
+        nsam = self.nsam
+        nchan = self.nchan
+        NCYCLES = self.NCYCLES
+        xoff = ch*nsam/NCYCLES/10
+        xx = np.array(range(-nsam/2-xoff,nsam/2-xoff))*NCYCLES*2*np.pi/nsam
+        return [ np.sin(x)/x if x != 0 else 1 for x in xx ]
+    
+    def __init__(self, uut, nchan, nsam):
+        self.uut = uut
+        self.nchan = nchan
+        self.nsam = nsam        
+        self.sw = self.sin()        
+        self.aw = np.zeros((nsam,nchan))
+        for ch in range(nchan):
+            self.aw[:,ch] = self.rainbow(ch)
+
+    
+    def generate(self):        
+        #for ch in range(self.nchan):
+        for ch in range(8):             # convenient to plot 8
+            aw1 = np.copy(self.aw)
+            aw1[:,ch] = np.add(np.multiply(self.sinc(ch),5),2)
+            print("loading array ", aw1.shape)
+            self.uut.load_awg((aw1*(2**15-1)/10).astype(np.int16))           
+            print("loaded array ", aw1.shape)
+            yield ch
         
 def run_shots(args):
     uut = acq400_hapi.Acq400(args.uuts[0])
@@ -24,6 +72,8 @@ def run_shots(args):
     
     if args.files != "":
         work = RunsFiles(uut, args.files.split(','))
+    else:
+        work = RainbowGen(uut, args.nchan, args.awglen)
         
     for ii in range(0, args.loop):
         print("shot: %d" % (ii))
@@ -42,6 +92,7 @@ def run_main():
     parser.add_argument('--loop', type=int, default=1, help="loop count")
     parser.add_argument('--capture', type=int, default=0, help="run a capture (assumes ADC present)")    
     parser.add_argument('--nchan', type=int, default=32, help='channel count for pattern')
+    parser.add_argument('--awglen', type=int, default=2048, help='samples in AWG waveform')
     parser.add_argument('--trg', default="int", help='trg "int|ext rising|falling"')
     parser.add_argument('uuts', nargs=1, help="uut ")
     run_shots(parser.parse_args())
