e5be258c4b01d31a1519b0bed839be702b149f29
Peter Milne <peter.milne@d-tacq.com>
2017-08-13 16:29:34 +0100

    hil.py hardware in the loop demo 	modified:   acq1001_awg_demo.py 	new file:   awg_data.py 	new file:   hil.py

--- hil.py
+++ hil.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Harware In Loop : load AO data,run a shot, get AI data, repeat.
 # upload to AWG and optionally run a capture.
 # data for upload is either File (host-local data file) or Rainbow, a test pattern.
 # assumes that clocking has been pre-assigned.
@@ -8,39 +9,44 @@ import acq400_hapi
 import awg_data
 import argparse
 
-        
+def store_file(it, rdata, nchan, nsam):
+    fn = 'DATA/ai%04d.dat' % (it)
+    print("store_file {}".format(fn))
+    
+    with open(fn, 'w') as f:        
+        f.write(rdata)
+
 def run_shots(args):
     uut = acq400_hapi.Acq400(args.uuts[0])
     acq400_hapi.cleanup.init()
-    if args.capture > 0:
-        uut.s0.transient = 'POST=%d SOFT_TRIGGER=%d' % \
-            (args.post, 1 if args.trg == 'int' else 0)
-        shot_controller = acq400_hapi.ShotController([uut])
     
+    uut.s0.transient = 'POST=%d SOFT_TRIGGER=%d DEMUX=0' % \
+            (args.post, 1 if args.trg == 'int' else 0)    
+
     for sx in uut.modules:
         uut.modules[sx].trg = '1,1,1'  if args.trg == 'int' else '1,0,1'
-    
+
     if args.files != "":
         work = awg_data.RunsFiles(uut, args.files.split(','))
     else:
         work = awg_data.RainbowGen(uut, args.nchan, args.awglen)
         
+    store = store_file
+    loader = work.load()
     for ii in range(0, args.loop):
         print("shot: %d" % (ii))
-            
-        for f in work.load():
-            print("Loaded %s" % (f))
-            if args.capture > 0:
-                shot_controller.run_shot(soft_trigger= True if args.trg=='int' else False)
-            else:
-                raw_input("hit return when done")
-            
+        f = loader.next()
+        print("Loaded %s" % (f))
+        uut.run_oneshot()
+        print("read_chan %d" % (args.post*args.nchan))
+        rdata = uut.read_chan(0, args.post*args.nchan)
+        store(ii, rdata, args.nchan, args.post)
+
 
 def run_main():
-    parser = argparse.ArgumentParser(description='acq1001 awg demo')
+    parser = argparse.ArgumentParser(description='acq1001 HIL demo')
     parser.add_argument('--files', default="", help="list of files to load")
-    parser.add_argument('--loop', type=int, default=1, help="loop count")
-    parser.add_argument('--capture', type=int, default=0, help="run a capture (assumes ADC present)")    
+    parser.add_argument('--loop', type=int, default=1, help="loop count")        
     parser.add_argument('--nchan', type=int, default=32, help='channel count for pattern')
     parser.add_argument('--awglen', type=int, default=2048, help='samples in AWG waveform')
     parser.add_argument('--post', type=int, default=100000, help='samples in ADC waveform')
@@ -52,6 +58,4 @@ def run_main():
 
 if __name__ == '__main__':
     run_main()
-
-
 
