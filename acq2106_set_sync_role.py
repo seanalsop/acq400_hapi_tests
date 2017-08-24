6cf40cdf478350d3e705d927b9b2aa2dc7acb965
Peter Milne <peter.milne@d-tacq.com>
2017-08-24 21:39:49 +0100

    flesh to full master_trg, master_clk control. nb: master_trg will hold fp trg DISABLED (by not selecting it), until all is ready (say when). expecting some combo of 2 scripts, one trigger control, one shot control 	modified:   acq2106_set_sync_role.py

--- acq2106_set_sync_role.py
+++ acq2106_set_sync_role.py
@@ -7,21 +7,55 @@ acq2106_set_sync_role master [slave1 ... slaveN]
 import argparse
 import acq400_hapi
 
+def set_mb_clk(uut, clkdef):
+    if (len(clkdef) == 3):
+        (src, hz, fin) = clkdef
+        uut.set_mb_clk(hz=hz, src=src, fin=fin)
+    else:
+        (src, hz) = clkdef
+        uut.set_mb_clk(hz=hz, src=src)
+    
+    
+def rf(edge):
+    return 1 if edge == "rising" else 0
 
 def run_main(parser):
-    uuts = [ acq400_hapi.Acq400(addr) for addr in parser.uuts ]      
-    
+    uuts = [ acq400_hapi.Acq2106(addr) for addr in parser.uuts ]      
     role = "master"
     
     for uut in uuts:
-        uut.set_sync_routing(role)
-        role = "slave"
+        uut.s0.trace = 1
+        if role == "master":
+            trg = "1,%d,%d" % (1 if parser.master_trg=="int" else 0, rf(parser.trg_edge))
+            
+            uut.set_sync_routing_master()
+            uut.set_master_trg(parser.master_trg, \
+                               enabled = True if parser.master_trg=="int" else False)
+            set_mb_clk(uut, parser.master_clk.split(','))            
+            role = "slave"
+        else:
+            trg = "1,%d,%d" % (0, rf(parser.trg_edge))
+            uut.set_sync_routing_slave()
+            
+        for s in uut.modules:
+            uut.modules[s].trg = trg
+            uut.modules[s].clk = '1,0,1'
+            uut.modules[s].clkdiv = parser.clkdiv
+     
+    if parser.master_trg != "int":
+         raw_input("say when")
+         uuts[0].set_master_trg(parser.master_trg, enabled=True)       
+        
 
 # execution starts here
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='acq2106_set_sync_role')
-    parser.add_argument('uuts', nargs='+', help="uut pairs: m1,m2 [s1,s2 ...]")
+    parser = argparse.ArgumentParser(description="acq2106_set_sync_role")
+    parser.add_argument("--master_clk", default="zclk,2000000", help="master_clk role alt fp,sysclk,sampleclk")
+    parser.add_argument("--master_trg", default="int", help="master_trg src alt fp")
+    parser.add_argument("--trg_edge", default="rising", help="selects trigger edge all modules")
+    parser.add_argument("--clkdiv", default="1", help="clock divider, each module")
+    parser.add_argument("uuts", nargs='+', help="uut pairs: m1 [s1 s2 ...]")
     run_main(parser.parse_args())
 
 
