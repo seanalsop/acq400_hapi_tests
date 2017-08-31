826b90c15fd72da94fc98e07a5ae232f1fef5a2c
dt100 <dt100@d-tacq.com>
2017-08-31 13:39:00 +0100

    sys() .. run in a loop 	modified:   hapi_proxy.py

--- hapi_proxy.py
+++ hapi_proxy.py
@@ -6,20 +6,33 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description='configure acq400_abort')
+parser.add_argument('--sys', default=0, help="run in interpreter loop N times")
 parser.add_argument('uut', nargs='+', help="uut")
 
 args = parser.parse_args()
-uuts = [ acq400_hapi.Acq400(u) for u in args.uut ]
+#uuts = [ acq400_hapi.Acq400(u) for u in args.uut ]
+uuts = [ acq400_hapi.Acq2106(u) for u in args.uut ]
 uut = uuts[0]
 
-def sys():
-    cmd = raw_input(">")
+def sys1(cmd):
     if cmd.find("=") > 0:
         for u in uuts:
             exec('u.{}'.format(cmd))
     else:
         for u in uuts:
             print("%s" % (eval('u.{}'.format(cmd))))
+            
+def sys(loop = 1):
+    while loop:
+        sys1(raw_input(">"))
+        loop -= 1
+        
+if args.sys > 0:
+    sys(loop=int(args.sys))
+
+        
+
+    
             
 
     
