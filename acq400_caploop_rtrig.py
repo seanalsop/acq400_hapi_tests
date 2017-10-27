0b3746b51b54330bb2465e78fa4adcba4581d9b7
Peter Milne <peter.milne@d-tacq.com>
2017-10-27 13:23:27 +0100

    capture loop controller it's assumed that the UUT's have already been preconfigured for capture. default trigger is a soft_trigger on UUT[0]  ./acq400_caploop_rtrig.py --trigger_uut=acq1001_315 acq2106_075 acq2106_075 set_arm acq2106_075 ARMED! acq1001_315 trigger acq2106_075 STOPPED! acq2106_075 SHOT COMPLETE shot:1 sleep(1.00) acq2106_075 set_arm acq2106_075 ARMED! acq1001_315 trigger acq2106_075 STOPPED! acq2106_075 SHOT COMPLETE shot:2 sleep(1.00) acq2106_075 set_arm
    .. in this case, trigger_uut has been configured to output a trigger pulse (on its own soft_trigger command), and this pulse is transmitted to the front panel
    of UUT[s].
    
    	new file:   acq400_caploop_rtrig.py

---
+++ acq400_caploop_rtrig.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+""" capture loop test
+    acq1001_caploop UUT1 [UUT2 ..]
+    where UUT1 is the ip-address or host name of first uut
+    example test client runs captures in a loop on one or more uuts
+    
+    pre-requisite: UUT's are configured and ready to make a transient
+    capture 
+    eg clk is running. soft trg enabled
+    eg transient length set.
+    
+    loop continues "forever" until <CTRL-C>
+"""
+
+import sys
+import acq400_hapi
+import argparse
+
+class RemoteTrigger:
+    def __init__(self, uut):
+        self.uut = acq400_hapi.Acq400(uut)
+    def __call__(self):
+        print("{} trigger".format(self.uut.uut))
+        self.uut.s0.soft_trigger = 1
+
+def run_shots(args):
+    uuts =  [ acq400_hapi.Acq400(u) for u in args.uuts ]
+
+    acq400_hapi.cleanup.init()
+
+    for uut in uuts:
+        if hasattr(uut.s0, 'TIM_CTRL_LOCK'):
+            print "LOCKDOWN {}".format(uut)
+            uut.s0.TIM_CTRL_LOCK = 1
+
+    shot_controller = acq400_hapi.ShotController(uuts)
+    shot = 0
+
+    if args.trigger_uut != None:
+        _remote_trigger = RemoteTrigger(args.trigger_uut)
+        _soft_trigger = False
+    else:
+        _remote_trigger = None
+        _soft_trigger = True
+    try:
+        while shot < args.nshots:
+            shot_controller.run_shot(remote_trigger=_remote_trigger,
+                    soft_trigger=_soft_trigger)
+            acq400_hapi.cleanup.sleep(1.0)
+            shot += 1
+            
+    except acq400_hapi.cleanup.ExitCommand:
+        print("ExitCommand raised and caught")
+    finally:
+        print("Finally, going down")
+
+
+def run_main():
+    parser = argparse.ArgumentParser(description='acq400 looptest')
+    parser.add_argument('--trigger_uut', default=None, type=str, help="external trigger uut eg ACQ1001+DIO, soft_trigger brought to front panel" )
+    parser.add_argument('--nshots', default=9999999, type=int, help="number of shots, default infinit")
+    parser.add_argument('uuts', nargs='+', help="uuts")
+    run_shots(parser.parse_args())
+
+# execution starts here
+
+if __name__ == '__main__':
+    run_main()
+
+
+
+
