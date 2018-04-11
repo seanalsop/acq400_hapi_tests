ca8ca7f8c7a901bc17e8e4f7ff2ff65d99cc74e9
Unknown <sean.alsop@d-tacq.co.uk>
2018-04-11 12:28:18 +0100

    Fixed bug: user must hit key to continue in mgtdramshot.py
    Added several flush() calls in the python script to fix the bug. Bug only existed on windows.

--- mgtdramshot.py
+++ mgtdramshot.py
@@ -17,31 +17,40 @@ class UploadFilter:
     def __init__(self):
         self.okregex = re.compile(r"axi0 start OK ([0-9]{4}) OK")
         self.line = 0
-        
+
     def __call__ (self, st):
 	st = st.rstrip()
+        sys.stdout.flush()
         LOG.write("{}\n".format(st))
-        
+        sys.stdout.flush()
+
         if self.okregex.search(st) != None:
 	    if self.line%10 != 0:
+                sys.stdout.flush()
                 sys.stdout.write('.')
             else:
+                sys.stdout.flush()
                 sys.stdout.write("{}".format(self.line/10))
             sys.stdout.flush()
             self.line += 1
+            sys.stdout.flush()
             if self.line > 100:
+                sys.stdout.flush()
                 sys.stdout.write('\n')
                 self.line = 0
         else:
             if self.line != 0:
+                sys.stdout.flush()
                 sys.stdout.write('\n')
+            sys.stdout.flush()
             print(">{}".format(st))
-            self.line = 0            
-   
-   
+            self.line = 0
+            sys.stdout.flush()
+
+
 def set_simulate(uut, enable):
     for s in uut.modules:
-        uut.modules[s].simulate = '1' if enable else '0'       
+        uut.modules[s].simulate = '1' if enable else '0'
 
 def run_shot(uut, args):
 	# always capture over. The offload is zero based anyway, so add another one
@@ -49,7 +58,7 @@ def run_shot(uut, args):
     uut.run_mgt()
     uut.s14.mgt_offload = args.offloadblocks if args.offloadblocks != 'capture' \
         else '0-{}'.format(args.captureblocks)
-	
+
     t1 = datetime.datetime.now()
     uut.run_mgt(UploadFilter())
     ttime = datetime.datetime.now()-t1
@@ -63,28 +72,28 @@ def run_shot(uut, args):
         if rc != 0:
             print("ERROR called process {} returned {}".format(args.validate, rc))
             exit(1)
-    
+
 def run_shots(args):
     global LOG
     LOG = open("mgtdramshot-{}.log".format(args.uut[0]), "w")
     uut = acq400_hapi.Acq2106(args.uut[0], has_mgtdram=True)
     uut.s14.mgt_taskset = '1'
     set_simulate(uut, args.simulate)
-    
+
     for ii in range(0, args.loop):
 	t1 = datetime.datetime.now()
         print("shot: {} {}".format(ii, t1.strftime("%Y%m%d %H:%M:%S")))
         run_shot(uut, args)
 	t2 = datetime.datetime.now()
 	print("done in {} seconds\n\n".format((t2-t1).seconds))
-        
+
         if args.wait_user:
             raw_input("hit return to continue")
 
 
 def run_main():
-    parser = argparse.ArgumentParser(description='acq2106 mgtdram test')    
-    parser.add_argument('--loop', type=int, default=1, help="loop count")     
+    parser = argparse.ArgumentParser(description='acq2106 mgtdram test')
+    parser.add_argument('--loop', type=int, default=1, help="loop count")
     parser.add_argument('--captureblocks', type=int, default="2000", help='number of 4MB blocks to capture')
     parser.add_argument('--offloadblocks', type=str, default="capture", help='block list to upload nnn-nnn')
     parser.add_argument('--validate', type=str, default='no', help='program to validate data')
