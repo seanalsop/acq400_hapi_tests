2570b0e7f7483b7d62d7ff439b8de7178dafd336
Unknown <sean.alsop@d-tacq.co.uk>
2018-04-25 12:10:56 +0100

    acq400_stream now collects recv data up to a user specified file size
    Data is collected by sock.recv and is stored until it reaches a user specified size, where it is then written to a dat file. For future commits it would be useful to have a time feature where the data is captured until EITHER time condition is met or file size exceeds user specification.

--- acq400_stream.py
+++ acq400_stream.py
@@ -15,20 +15,24 @@ import argparse
 
 # make_data_dir creates a subdirectory
 # where data is to be stored.
-def make_data_dir(direc):
+def make_data_dir(directory, verbose):
     try:
-        os.mkdir(direc)
+        os.mkdir(directory)
     except Exception:
+        if verbose:
+            print "Exception met while creating data directory: Passing"
         pass
 
 
 def run_stream(args):
+
+    data = ""
     num = 0
     uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
 
     for uut in uuts:
         channum = uut.s0.NCHAN
-        print(int(channum))
+        # print(int(channum))
         try:
             if uut.s0.data32:
                 wordsizetype = "<i4"  # 32 bit little endian
@@ -39,31 +43,47 @@ def run_stream(args):
             wordsize = 16
 
         skt = acq400_hapi.Netclient(args.uuts[0], 4210)  # Needs to be string
+        make_data_dir(args.root, args.verbose)
         start_time = time.clock()
-        make_data_dir(args.root)
+        upload_time = time.clock()
 
         while time.clock() < (start_time + args.runtime):
 
-            data = skt.sock.recv(args.filesamples*wordsize*(int(channum)))
-            data_file = open("{}/data{}.dat".format(args.root, num), "wb")
-            data = np.frombuffer(data, dtype=wordsizetype, count=-1)
+            loop_time = time.clock()
+            data += skt.sock.recv(1024)
 
-            if args.plot == 1:
-                plt.plot(data[::32])
-                plt.show()
+            if len(data) / 512 >= args.filesize:
 
-            data.tofile(data_file, '')
-            num += 1
-            data_file.close()
+                data_file = open("{}/data{}.dat".format(args.root, num), "wb")
+                data = np.frombuffer(data, dtype=wordsizetype, count=-1)
+
+                if args.plot == 1:
+                    plt.plot(data[::32])
+                    plt.show()
+
+                data.tofile(data_file, '')
+
+                if args.verbose == 1:
+                    print "New data file written."
+                    print "Data Transferred: ", float(len(data))/1024, "KB"
+                    print "loop_time: ", loop_time
+                    print "Data upload & save rate: ", float(len(data))/1024/((time.clock()-upload_time)), "KB/s"
+
+                num += 1
+                data_file.close()
+
+                data = "" # Remove data from variable once it has been written
+                upload_time = time.clock() # Reset upload time
+
 
 
 def run_main():
     parser = argparse.ArgumentParser(description='acq400 stream')
-    parser.add_argument('--filesamples', default=10, type=int, help="Number of samples to store in file")
+    parser.add_argument('--filesize', default=1024, type=int, help="Size of file to store in KB")
     parser.add_argument('--root', default="ROOT", type=str, help="Location to save files")
-    parser.add_argument('--runtime', default=5, type=int, help="How long to stream data for")
+    parser.add_argument('--runtime', default=10, type=int, help="How long to stream data for")
     parser.add_argument('--plot', default=0, type=int, help='Whether the data streamed from the loop is plotted - pauses stream')
-    parser.add_argument('--verbose', default=1, type=int, help='Prints status messages as the stream is running')
+    parser.add_argument('--verbose', default=0, type=int, help='Prints status messages as the stream is running')
     parser.add_argument('uuts', nargs='+', help="uuts")
 
     run_stream(parser.parse_args())
