65d09ceb3f461592390e4c2538c8e3f6568714c3
Unknown <sean.alsop@d-tacq.co.uk>
2018-04-26 12:43:18 +0100

    Removed some dev features and cleaned up file.
    Removed print statements and cleaned the file.

--- acq400_stream.py
+++ acq400_stream.py
@@ -1,63 +1,49 @@
 #!/usr/bin/env python
 
 """
-This is a script intended to connect to a UUT and
-stream data from port 4210.
-
-The data that has been streamed is not demuxed and
-so if it is to be used then it has to be demuxed first.
-Something like:
+This is a script intended to connect to a UUT and stream data from port 4210. The data that has been streamed is not
+demuxed and so if it is to be used then it has to be demuxed first. Something like:
 
 data = numpy.fromfile("data0.dat", dtype="<datatype>")
 plt.plot(data[::<number of channels>])
 plt.show()
 
 Notes:
-If filesize > total data then no data will be stored.
-If runtime is too short then no data will be stored.
-
-The default runtime is large so that the user can
-specify total data required rather than time to run.
-If a runtime is more applicable then specify a large
-total data and the run time required.
+If filesize > total data then filesize data will be stored in one file.
+The default runtime is large so that the user can specify total data required rather than time to run. If a runtime is
+more applicable then specify a large total data and the run time required.
 """
 
 import acq400_hapi
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import time
 import argparse
 
 
-# make_data_dir creates a subdirectory
-# where data is to be stored.
 def make_data_dir(directory, verbose):
     try:
         os.mkdir(directory)
     except Exception:
         if verbose:
-            print "Exception met while creating data directory: Passing"
+            print "Directory already exists"
         pass
 
 
 def run_stream(args):
-
     data = ""
     num = 0
     uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
 
     for uut in uuts:
-
         try:
             if uut.s0.data32:
                 wordsizetype = "<i4"  # 32 bit little endian
-
         except AttributeError:
             print("Attribute error detected. No data32 attribute - defaulting to 16 bit")
             wordsizetype = "<i2"  # 16 bit little endian
 
-        skt = acq400_hapi.Netclient(args.uuts[0], 4210)  # Needs to be string
+        skt = acq400_hapi.Netclient(args.uuts[0], 4210)
         make_data_dir(args.root, args.verbose)
         start_time = time.clock()
         upload_time = time.clock()
@@ -72,11 +58,6 @@ def run_stream(args):
                 data_length += float(len(data)) / 1024
                 data_file = open("{}/data{}.dat".format(args.root, num), "wb")
                 data = np.frombuffer(data, dtype=wordsizetype, count=-1)
-
-                if args.plot == 1:
-                    plt.plot(data[::32])
-                    plt.show()
-
                 data.tofile(data_file, '')
 
                 if args.verbose == 1:
@@ -86,6 +67,7 @@ def run_stream(args):
                     print "Data upload & save rate: ", float(len(data))/1024/((time.clock()-upload_time)), "KB/s"
                     print ""
                     print ""
+
                 num += 1
                 data_file.close()
                 data = "" # Remove data from variable once it has been written
@@ -110,7 +92,6 @@ def run_main():
     parser.add_argument('--plot', default=0, type=int, help='Whether the data streamed from the loop is plotted - pauses stream')
     parser.add_argument('--verbose', default=0, type=int, help='Prints status messages as the stream is running')
     parser.add_argument('uuts', nargs='+', help="uuts")
-
     run_stream(parser.parse_args())
 
 
