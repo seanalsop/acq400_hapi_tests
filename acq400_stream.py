5d075dd46ed655187d74030c4a5cefdae466cfe3
Unknown <sean.alsop@d-tacq.co.uk>
2018-04-26 11:05:34 +0100

    Added ability to run stream by total data collected OR by stream time
    acq400_stream.py now stops the stream after a total amount of data has been streamed or by time spent streaming. A bug was also fixed where the amount of data stored was not equal to the filesize parameter.

--- acq400_stream.py
+++ acq400_stream.py
@@ -3,6 +3,23 @@
 """
 This is a script intended to connect to a UUT and
 stream data from port 4210.
+
+The data that has been streamed is not demuxed and
+so if it is to be used then it has to be demuxed first.
+Something like:
+
+data = numpy.fromfile("data0.dat", dtype="<datatype>")
+plt.plot(data[::<number of channels>])
+plt.show()
+
+Notes:
+If filesize > total data then no data will be stored.
+If runtime is too short then no data will be stored.
+
+The default runtime is large so that the user can
+specify total data required rather than time to run.
+If a runtime is more applicable then specify a large
+total data and the run time required.
 """
 
 import acq400_hapi
@@ -46,13 +63,14 @@ def run_stream(args):
         make_data_dir(args.root, args.verbose)
         start_time = time.clock()
         upload_time = time.clock()
+        data_length = 0
 
-        while time.clock() < (start_time + args.runtime):
+        while time.clock() < (start_time + args.runtime) and data_length < args.totaldata:
 
             loop_time = time.clock()
             data += skt.sock.recv(1024)
 
-            if len(data) / 512 >= args.filesize:
+            if len(data) / 1024 >= args.filesize:
 
                 data_file = open("{}/data{}.dat".format(args.root, num), "wb")
                 data = np.frombuffer(data, dtype=wordsizetype, count=-1)
@@ -62,10 +80,11 @@ def run_stream(args):
                     plt.show()
 
                 data.tofile(data_file, '')
+                data_length += float(len(data)) / 1024
 
                 if args.verbose == 1:
                     print "New data file written."
-                    print "Data Transferred: ", float(len(data))/1024, "KB"
+                    print "Data Transferred: ", data_length, "KB"
                     print "loop_time: ", loop_time
                     print "Data upload & save rate: ", float(len(data))/1024/((time.clock()-upload_time)), "KB/s"
 
@@ -79,9 +98,10 @@ def run_stream(args):
 
 def run_main():
     parser = argparse.ArgumentParser(description='acq400 stream')
-    parser.add_argument('--filesize', default=1024, type=int, help="Size of file to store in KB")
+    parser.add_argument('--filesize', default=2048, type=int, help="Size of file to store in KB. If filesize > total data then no data will be stored.")
+    parser.add_argument('--totaldata', default=4096, type=int, help="Total amount of data to store in KB")
     parser.add_argument('--root', default="ROOT", type=str, help="Location to save files")
-    parser.add_argument('--runtime', default=10, type=int, help="How long to stream data for")
+    parser.add_argument('--runtime', default=1000, type=int, help="How long to stream data for")
     parser.add_argument('--plot', default=0, type=int, help='Whether the data streamed from the loop is plotted - pauses stream')
     parser.add_argument('--verbose', default=0, type=int, help='Prints status messages as the stream is running')
     parser.add_argument('uuts', nargs='+', help="uuts")
